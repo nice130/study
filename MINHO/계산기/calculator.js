16d6b5fd6e8898144c15629adb270981b46fb767
minosssss <mh.lee239@gmail.com>
2022-09-11 21:56:13 +0900

    계산 로직 수정
    TODO
    1. 계산 목록 보여주기
    2. 로직 최적화

--- MINHO/계산기/calculator.js
+++ MINHO/계산기/calculator.js
@@ -9,39 +9,74 @@ const $comma = document.querySelector("#comma");
 const $equal = document.querySelector("#equal");
 let checkBtn = false
 
-function onClickNumber () {
-    if (calObj.checkBtn) {
-      $InputBox.value = ""
-    }
-    const textValue = this.innerHTML;
-    $displayBox.innerHTML   += textValue;
-    $InputBox.value   += textValue;
-    checkBtn = false
+// function onClickNumber () {
+//     if (calObj.preOp)
+//     if (calObj.checkBtn) {
+//       $InputBox.value = ""
+//     }
+//     const textValue = this.innerHTML;
+//     $displayBox.innerHTML   += textValue;
+//     $InputBox.value   += textValue;
+//     checkBtn = false
    
+// }
+
+// function onClickOperator () {
+//     const operatorValue = this.innerHTML;
+//     $displayBox.innerHTML   += operatorValue;
+//     calObj.op = operatorValue;
+//     checkBtn = true;
+//     if (!calObj.checkPreNum) {
+//       calObj.preNum = Number($InputBox.value)
+//       console.log(calObj.preNum)
+//       calObj.checkPreNum = true
+//     } else {
+//       calObj.nextNum = Number($InputBox.value)
+//       console.log(calObj.nextNum)
+//       $InputBox.value = "";
+//       calObj.resultFn(calObj.preOp)
+//       calObj.preNum = calObj.result
+//       if (calObj.op == '=') {
+//         calObj.checkPreNum = false
+//         $displayBox.innerHTML += calObj.result +', '
+//       }
+//     }
+//     calObj.preOp = calObj.op;
+// }
+
+const onClickNumber = (e) => {
+  const numberValue = e.target.textContent;
+  $displayBox.innerHTML += numberValue;
+  if (!calObj.op) {
+    calObj.preNum += numberValue;
+    $InputBox.value += numberValue;
+    return;
+  }
+  if (!calObj.nextNum) {
+    $InputBox.value = '';
+  }
+  calObj.nextNum += numberValue;
+  $InputBox.value += numberValue;
+
 }
 
-function onClickOperator () {
-    const operatorValue = this.innerHTML;
-    $displayBox.innerHTML   += operatorValue;
-    calObj.op = operatorValue;
-    checkBtn = true;
-    if (!calObj.checkPreNum) {
-      calObj.preNum = Number($InputBox.value)
-      console.log(calObj.preNum)
-      calObj.checkPreNum = true
-    } else {
-      calObj.nextNum = Number($InputBox.value)
-      console.log(calObj.nextNum)
-      $InputBox.value = "";
-      calObj.resultFn(calObj.preOp)
-      calObj.preNum = calObj.result
-      if (calObj.op == '=') {
-        calObj.checkPreNum = false
-        $displayBox.innerHTML += calObj.result +', '
-      }
-    }
-    calObj.preOp = calObj.op;
+const onClickOperator = (e) => {
+  const operatorValue = e.target.value;
+  $displayBox.innerHTML += operatorValue;
+  if (calObj.preNum) {
+      calObj.op = operatorValue;
+  }
+  if (calObj.nextNum) {
+    calObj.resultFn(operatorValue);
+  }
 }
+
+const onClearValue = () => {
+  $InputBox.value = "";
+  $displayBox.innerHTML = "";
+  calObj.result, calObj.preNum, calObj.nextNum = '';
+}
+
 // // 숫자버튼 클릭시 inputNum함수 호출
 for (let i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener('click', onClickNumber);
@@ -56,13 +91,13 @@ $comma.addEventListener('click', function() {
     log(this.innerHTML);
 })
 
+$clearBtn.addEventListener('click', onClearValue);
+
 let calObj = {
-  result : 0,
-  preNum : 0,
-  checkPreNum : false,
-  nextNum : 0,
-  preObj : null,
-  op : null,
+  result : '',
+  preNum : '',
+  nextNum : '',
+  op : '',
   resultFn: function(op) {
     switch (op) {
     case '+':
@@ -82,64 +117,11 @@ let calObj = {
     console.log("result"+calObj.result)
     break;
     }
-    $displayBox.value = calObj.result
+    onClearValue();
+    calObj.preNum = calObj.result;
+    $InputBox.value = calObj.result;
   }
 }
 
-// // 계산기 객체
-// let cal = {
-//   result: 0,
-//   preNum: 0,
-//   nextNum: 0,
-//   preOp: null,
-//   op: null,
-//   calculator: function() {
-//     var inputText = this.innerHTML
-//     cal.op = inputText
-//     showBox.innerHTML += inputText
-//     checkBtn = true
-//     if (!checkPreNum) {
-//       cal.preNum = Number(numInput.value)
-//       console.log(cal.preNum)
-//       checkPreNum = true
-//     } else {
-//       cal.nextNum = Number(numInput.value)
-//       console.log(cal.nextNum)
-//       clearNum()
-//       cal.resultFn(cal.preOp)
-//       cal.preNum = cal.result
-//       if (cal.op == '=') {
-//         checkPreNum = false
-//         showBox.innerHTML += cal.result +', '
-//       }
-//     }
-//     cal.preOp = cal.op
-//   },
-//   resultFn: function(op) {
-//     switch (op) {
-//     case '+':
-//     cal.result = cal.preNum + cal.nextNum
-//     console.log("result"+cal.result)
-//     break;
-//     case '-':
-//     cal.result = cal.preNum - cal.nextNum
-//     console.log("result"+cal.result)
-//     break;
-//     case '*':
-//     cal.result = cal.preNum * cal.nextNum
-//     console.log("result"+cal.result)
-//     break;
-//     case '/':
-//     cal.result = cal.preNum / cal.nextNum
-//     console.log("result"+cal.result)
-//     break;
-//     }
-//     numInput.value = cal.result
-//   }
-// }
 
 
-$clearBtn.addEventListener('click', function() {
-  $InputBox.value = "";
-  $displayBox.innerHTML = "";
-})

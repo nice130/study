de709a3d97e39f042a584170d4f1a2bd024770aa
minosssss <mh.lee239@gmail.com>
2022-10-30 15:20:20 +0900

    about useEffect

--- MINHO/React/react-for-beginners-npx/src/App.js
+++ MINHO/React/react-for-beginners-npx/src/App.js
@@ -1,11 +1,34 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-
+// import Button from "./Button";
+// import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+  console.log("I run all the time");
+  const OnlyOnce = () => {
+    console.log("CALL THE API...");
+  };
+  useEffect(() => {
+    OnlyOnce();
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5)
+    console.log("SEARCH FOR", keyword);
+  }, [keyword]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      {/* <h1 className={styles.title}>Welcome back!</h1> */}
+      {/* <Button text={"Continue"}/> */}
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me!</button>
     </div>
   );
 }
--- MINHO/React/react-for-beginners-npx/src/Button.js
+++ MINHO/React/react-for-beginners-npx/src/Button.js
@@ -2,7 +2,10 @@ import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 
 function Button({text}) {
-  return <button className={styles.btn}>{text}</button>
+  return <button
+  className={styles.btn}
+  >{text}
+  </button>
 }
 
 Button.propTypes = {

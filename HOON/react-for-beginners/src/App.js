5afed4b86de26f07ba3d9c1c9edfdeef9ad8a2bc
nice130 <nice130@naver.com>
2022-10-31 23:57:10 +0900

    저장

--- HOON/react-for-beginners/src/App.js
+++ HOON/react-for-beginners/src/App.js
@@ -1,17 +1,26 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import {useState, useEffect} from "react";
+
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("")
   const onClick = () => setValue((prev)=>prev +1);
-  console.log("i run all the time");
+  const onChange = (e) =>setKeyword(e.target.value);
   const iRunOnlyOnce = ()=>{
     console.log("i run only once.");
   };
-  useEffact(iRunOnlyOnce,[]);
+  useEffect(iRunOnlyOnce,[]);
+  // useEffact(()=>{
+  //   console.log("CALL THE API....");
+  // },[]);
   return (
     <div>
+      <input 
+        value={keyword}
+        onChange={onChange} 
+        type="text" 
+        placeholder="Search here....." />
       <h1>{counter}</h1>
-      <butotn onClick ={onClick}>click</butotn>
+      <button onClick ={onClick}>click</button>
     </div>
   );    
 }

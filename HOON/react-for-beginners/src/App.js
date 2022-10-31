8afedf5585f1f3a8df73dabd92a7c6724f60e2c0
nice130 <nice130@naver.com>
2022-10-31 17:47:18 +0900

    고고

--- HOON/react-for-beginners/src/App.js
+++ HOON/react-for-beginners/src/App.js
@@ -1,10 +1,17 @@
 import Button from "./Button";
 import styles from "./App.module.css";
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev)=>prev +1);
+  console.log("i run all the time");
+  const iRunOnlyOnce = ()=>{
+    console.log("i run only once.");
+  };
+  useEffact(iRunOnlyOnce,[]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      <h1>{counter}</h1>
+      <butotn onClick ={onClick}>click</butotn>
     </div>
   );    
 }

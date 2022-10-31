b358e9c9f5e3ddbd4e7aaf96f668e328abb0d36e
minosssss <mh.lee239@gmail.com>
2022-10-31 21:17:28 +0900

    get CoinAPI and input amount USD

--- MINHO/React/react-for-beginners-npx/src/App.js
+++ MINHO/React/react-for-beginners-npx/src/App.js
@@ -1,43 +1,50 @@
-// import Button from "./Button";
-// import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (e) => setToDo(e.target.value);
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currnet) => [...currnet, toDo]);
-    setToDo("");
-  };
-  const deleteBtn = (targetIdx) => {
-    setToDos(toDos.filter((_, idx) => idx !== targetIdx));
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [cost, setCost] = useState(0);
+  const [need, setNeed] = useState(0);
+  function handleInput(e) {
+    setNeed(e.target.value);
+  }
+  function onChange(e) {
+    setCost(e.target.value);
+    setNeed(1);
+  }
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response)=> response.json())
+    .then((json)=> { 
+      setCoins(json);
+      setLoading(false);
+    });
+
+  },[]);
   return (
     <div>
-      <h1>My ToDoList{toDos.length === 0 ? null : "(" + toDos.length + ")"}</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
+        <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+      <strong>Loading...</strong>) 
+      : (
+      <select onChange={onChange}>
+        {coins.map((coin) => 
+        <option key={coin.id} value={coin.quotes.USD.price}>
+          {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+          </option>)}
+      </select>
+      )}
+      <h2>Enter the amount</h2>
+      <div>
+        $<input 
+        type="number"
+        value={need}
+        placeholder="dollor"
+        onChange={handleInput}
         />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, idx) => (
-          <li key={idx}>
-            {item}
-            <button onClick={() => deleteBtn(idx)}>❌</button>
-          </li>
-        ))}
-      </ul>
+      </div>
+      <h3>{cost !== 0 ? `You can get ${need/cost}` : null} </h3>
     </div>
   );
 }
+
 export default App;

00875cc350df27eaf408175384eb7e05ba9733e6
nice130 <nice130@naver.com>
2022-11-01 17:57:23 +0900

    1101

--- HOON/react-for-beginners/src/App.js
+++ HOON/react-for-beginners/src/App.js
@@ -1,28 +1,19 @@
 import {useState, useEffect} from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("")
-  const onClick = () => setValue((prev)=>prev +1);
-  const onChange = (e) =>setKeyword(e.target.value);
-  const iRunOnlyOnce = ()=>{
-    console.log("i run only once.");
-  };
-  useEffect(iRunOnlyOnce,[]);
-  // useEffact(()=>{
-  //   console.log("CALL THE API....");
-  // },[]);
-  return (
-    <div>
-      <input 
-        value={keyword}
-        onChange={onChange} 
-        type="text" 
-        placeholder="Search here....." />
-      <h1>{counter}</h1>
-      <button onClick ={onClick}>click</button>
-    </div>
-  );    
+function Hello(){
+  useEffect(() =>{
+    console.log('create :)');
+  },[]);
+  return <h1>Hello</h1>
 }
 
+function App() {
+ const [showing, setShowing] = useState(false);
+ const onClick = () => setShowing((prev)=> !prev);
+ return <div>
+  {showing ? <Hello /> : null}
+  <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+ </div>
+}
+//6분부터 고고
 export default App;

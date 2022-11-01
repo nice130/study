9f7475de5173f5779a1807bd8211ae447688e591
nice130 <nice130@naver.com>
2022-11-02 00:45:44 +0900

    todos

--- HOON/react-for-beginners/src/App.js
+++ HOON/react-for-beginners/src/App.js
@@ -1,19 +1,31 @@
-import {useState, useEffect} from "react";
-
-function Hello(){
-  useEffect(() =>{
-    console.log('create :)');
-  },[]);
-  return <h1>Hello</h1>
-}
+import {useState} from "react";
 
 function App() {
- const [showing, setShowing] = useState(false);
- const onClick = () => setShowing((prev)=> !prev);
- return <div>
-  {showing ? <Hello /> : null}
-  <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
- </div>
+  const [toDo,setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) =>{
+    e.preventDefault();
+    if(toDo ===""){
+      return;
+    }
+    setToDo("");
+    setToDos(currentArray=> [toDo, ...currentArray]);
+  };
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do" />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
-//6분부터 고고
 export default App;

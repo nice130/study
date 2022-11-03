ecbc49f7b89ae851c4b2e8d7fb808b06a4af3f23
nice130 <nice130@naver.com>
2022-11-04 00:24:41 +0900

    usestate여러개

--- project/planning/src/App.js
+++ project/planning/src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import styles from'./styles.module.css';
 function App() {
+  const {loading,setloading} = useState (true);
+  const {values,setValues} = useState({
+    status:'',
+    uname:'',
+    area:'',
+    year:'',
+    date:'',
+    progress:'',
+  });
+  const {
+    status, uname, area, year, date, progress
+  } = values;
+
+  const onChange = (e)=>{
+    const {name, value} = e.target;
+     setValues = {
+      ...values,
+      [name] : value,
+    }
+    
+  };
+
+  const onClick = (e) =>{
+    const {name, value} = e.target;
+    console.log(values.status);
+    setValues('');
+  }
+  // setloading(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? <h1>Loading...</h1> : <div>PLANNING</div>}
+      <tbody>
+          <th rowSpan="3" className={styles.th}>GOALS</th>
+              <tr>
+                <td className={styles.td}>Status</td>
+                <td className={styles.td}>Name</td>
+                <td className={styles.td}>Area</td>
+                <td className={styles.td}>Year</td>
+                <td className={styles.td}>Date</td>
+                <td className={styles.td}>Progress</td>
+              </tr>
+              <tr>
+                <td className={styles.td}>
+                  <input name ="status" onChange={onChange} value={status} />
+                </td>
+                <td className={styles.td}>
+                  <input name = "uname" onChange={onChange} value={uname}/>
+                </td>
+                <td className={styles.td}>
+                  <input name = "area" onChange={onChange} value={area}/>
+                </td>
+                <td className={styles.td}>
+                  <input name = "year" onChange={onChange} value={year}/>
+                </td>
+                <td className={styles.td}>
+                  <input name = "date" onChange={onChange} value={date}/>
+                </td>
+                <td className={styles.td}>
+                  <input name = "progress" onChange={onChange} value={progress}/>
+                </td>
+                <button onClick={onClick}>입력</button>
+              </tr>
+              
+        </tbody>
     </div>
   );
 }

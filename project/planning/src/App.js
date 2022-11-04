5997c6d6de9b1a41faea7a2b47796e5261e1216c
nice130 <nice130@naver.com>
2022-11-04 17:49:47 +0900

    return에추가하기

--- project/planning/src/App.js
+++ project/planning/src/App.js
@@ -2,8 +2,9 @@ import { useState } from 'react';
 import './App.css';
 import styles from'./styles.module.css';
 function App() {
-  const {loading,setloading} = useState (true);
-  const {values,setValues} = useState({
+  // const {loading,setloading} = useState (true);
+  const [saveValues,setSaveVluese] = useState();
+  const [values,setValues] = useState({
     status:'',
     uname:'',
     area:'',
@@ -17,25 +18,46 @@ function App() {
 
   const onChange = (e)=>{
     const {name, value} = e.target;
-     setValues = {
+     setValues ({
       ...values,
       [name] : value,
-    }
-    
+    })
   };
-
-  const onClick = (e) =>{
-    const {name, value} = e.target;
-    console.log(values.status);
-    setValues('');
+  
+  const addtr = () =>{
+    return(
+      <tr>
+          <td className={styles.td}>{status}</td>
+          <td className={styles.td}>{uname}</td>
+          <td className={styles.td}>{area}</td>
+          <td className={styles.td}>{year}</td>
+          <td className={styles.td}>{date}</td>
+          <td className={styles.td}>{progress}</td>
+      </tr>
+    );
   }
+
+  const onClick = () =>{
+    setValues(values);
+    setValues({
+      status:'',
+      uname:'',
+      area:'',
+      year:'',
+      date:'',
+      progress:'',
+    })
+    console.log({status});
+    addtr();
+  };
   // setloading(false);
   return (
     <div>
-      {loading ? <h1>Loading...</h1> : <div>PLANNING</div>}
+     <div>PLANNING</div>
       <tbody>
-          <th rowSpan="3" className={styles.th}>GOALS</th>
+          
               <tr>
+                <td rowSpan="3" className={styles.th}>GOALS</td>
                 <td className={styles.td}>Status</td>
                 <td className={styles.td}>Name</td>
                 <td className={styles.td}>Area</td>
@@ -45,22 +67,22 @@ function App() {
               </tr>
               <tr>
                 <td className={styles.td}>
-                  <input name ="status" onChange={onChange} value={status} />
+                  <input name ="status" onChange={onChange} value={status} type='text'/>
                 </td>
                 <td className={styles.td}>
-                  <input name = "uname" onChange={onChange} value={uname}/>
+                  <input name = "uname" onChange={onChange} value={uname} type='text'/>
                 </td>
                 <td className={styles.td}>
-                  <input name = "area" onChange={onChange} value={area}/>
+                  <input name = "area" onChange={onChange} value={area} type='text'/>
                 </td>
                 <td className={styles.td}>
-                  <input name = "year" onChange={onChange} value={year}/>
+                  <input name = "year" onChange={onChange} value={year} type='text'/>
                 </td>
                 <td className={styles.td}>
-                  <input name = "date" onChange={onChange} value={date}/>
+                  <input name = "date" onChange={onChange} value={date} type='text'/>
                 </td>
                 <td className={styles.td}>
-                  <input name = "progress" onChange={onChange} value={progress}/>
+                  <input name = "progress" onChange={onChange} value={progress} type='text'/>
                 </td>
                 <button onClick={onClick}>입력</button>
               </tr>

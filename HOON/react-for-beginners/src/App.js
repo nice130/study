2ee771cb68ed158fa6907d55aa6a3a1baa0aa351
nice130 <nice130@naver.com>
2022-11-05 17:41:31 +0900

    reactRouter

--- HOON/react-for-beginners/src/App.js
+++ HOON/react-for-beginners/src/App.js
@@ -1,32 +1,19 @@
-import {useEffect, useState} from "react";
-import Movie from "./Movie"
+import {BrowserRouter as Router,Switch,Route,} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading,setloading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await ( 
-      await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setloading(false);
-  };
-  useEffect(()=>{
-    getMovies();
-  },[]);
-  console.log(movies);
   return (
-    <div>{loading ? (<h1>Loading...</h1>) 
-  :(<div> 
-      {movies.map((movie) => (
-      <Movie coverImg={movie.medium_cover_image}  title={movie.title}
-      summary ={movie.summary} genres = {movie.genres}/>)
-    )} 
-    </div> 
-  )}
-  </div>
+  <Router>
+    <Switch>
+      <Route path="/movie">
+        <Detail />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
   );
 }
 export default App;
---
+++ HOON/react-for-beginners/src/routes/Home.js
@@ -0,0 +1,33 @@
+import Movie from "../components/Movie";
+import {useState, useEffect} from "react";
+
+function Home(){
+  const [loading,setloading] = useState(true);
+  const [movies, setMovies] = useState([]);
+   const getMovies = async () => {
+     const json = await ( 
+       await fetch(
+       `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+       )
+     ).json();
+     setMovies(json.data.movies);
+     setloading(false);
+   };
+   useEffect(()=>{
+     getMovies();
+   },[]);
+   console.log(movies);
+   return (
+     <div>{loading ? (<h1>Loading...</h1>) 
+   :(<div> 
+       {movies.map((movie) => (
+       <Movie coverImg={movie.medium_cover_image}  title={movie.title}
+       summary ={movie.summary} genres = {movie.genres}/>)
+     )} 
+     </div> 
+   )}
+   </div>
+   );
+}
+
+export default Home;

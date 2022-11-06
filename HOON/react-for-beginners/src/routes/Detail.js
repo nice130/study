9c65f95411923f543549d1a50c58b534f3592bac
nice130 <nice130@naver.com>
2022-11-06 13:08:44 +0900

    커밋

--- HOON/react-for-beginners/src/routes/Detail.js
+++ HOON/react-for-beginners/src/routes/Detail.js
@@ -1,4 +1,8 @@
+import {useEffect} from "react";
+import { useParams } from "react-router-dom";
 function Detail(){
+    const {id} = useParams();
+    console.log(id);
     return <h1>Detail</h1>
 }
 export default Detail;

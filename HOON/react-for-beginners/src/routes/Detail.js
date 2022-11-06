da4a4c9a46c8d5f3403610e1bfffe36a77867294
HOON <nice130@naver.com>
2022-11-06 15:21:58 +0900

    react-router-dom설치

--- HOON/react-for-beginners/src/routes/Detail.js
+++ HOON/react-for-beginners/src/routes/Detail.js
@@ -1,8 +1,8 @@
-import {useEffect} from "react";
+// import {useEffect} from "react";
 import { useParams } from "react-router-dom";
 function Detail(){
-    const {id} = useParams();
-    console.log(id);
+    const x = useParams();
+    console.log(x);
     return <h1>Detail</h1>
 }
 export default Detail;

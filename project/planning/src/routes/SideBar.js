33c641f23d2bb47e63d32d4e93395607910d403f
minosssss <mh.lee239@gmail.com>
2022-11-23 20:27:16 +0900

    new sidebar css 적용

--- project/planning/src/routes/SideBar.js
+++ project/planning/src/routes/SideBar.js
@@ -1,88 +1,87 @@
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import styles from "./sidebar.module.css";
+// import styles from "./sidebar.module.css";
+import styles from "./new-sidebar.module.css";
 import NewNote from "./NewNote";
-const SideBar = ({width=250, setMainSize,mainSize})=>{
-    const [modalOpen, setModalOpen] = useState(false);
-    const [isOpen, setOpen] = useState(true);
-    const [xPosition, setX] = useState(0);
-    const side = useRef();
-    const [plan,setPlan] = useState("");
-    const [savePlan,setSavePlan] = useState([]);
 
-    const toggleMenu = () =>{
-        if(xPosition>0){
-            setX(0);
-            setOpen(true);
-            setMainSize(true);
-        }else{
-            setX(width);
-            setOpen(false); 
-            setMainSize(false);
-        }
-    };
-//밖에클릭하면 창닫히는기능 사용시 useEffect 추가
-    // const handlClose = async e =>{
-    //     let sideArea = side.current;
-    //     let sideCildren = side.current.contains(e.target);
-    //     if(isOpen && (!sideArea || !sideCildren)){
-    //         await setX(width);
-    //         await setOpen(false);
-    //     }
-    // }
-
-    // useEffect(()=>{
-    //     window.addEventListener('click',handlClose);
-    //     return ()=>{
-    //         window.removeEventListener('click',handlClose);
-    //     };
-    // })
-    const showModal=()=>{
-        setModalOpen(true); 
+const SideBar = ({ width = 250, setMainSize, mainSize }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [isOpen, setOpen] = useState(true);
+  const [xPosition, setX] = useState(0);
+  const side = useRef();
+  const [plan, setPlan] = useState("");
+  const [savePlan, setSavePlan] = useState([]);
+  
+  const toggleMenu = () => {
+    setMainSize(mainSize ? false : true);
+    if (xPosition > 0) {
+      setX(0);
+      setOpen(true);
+    } else {
+      setX(width);
+      setOpen(false);
     }
-    
+  };
+  const showModal = () => {
+    setModalOpen(true);
+  };
 
-    return (
-        <div className={styles.container}>
-          <div ref={side}  className={styles.sidebar} style={{ width: `${width}px`, height: '100%',  transform: `translatex(${-xPosition}px)`}}>
-              <button onClick={() => toggleMenu()}
-              className={styles.button} >
-                {isOpen ? 
-                <span>X</span> : <span>O</span>
-                }
-              </button>
-            
-            <div className={styles.content}>
-                <Link to={'/'}>PLAN LIST</Link>
-                <button className={styles.plus} onClick={showModal}>+</button>
-                
-                <ul>
-                    <Link to={`/planning`}>
-                        <li>planning</li>
-                    </Link>
-                    {savePlan.map((item,idx)=>(
-                        <Link to={`/planning`} key={idx} title={item} >
-                            <li value={item}>{item}</li></Link>
-                    ))}
-                    {plan}
-                </ul>
-            </div>
-            <div className={styles.modal}>
-                    {modalOpen && 
-                    <NewNote 
-                    setModalOpen={setModalOpen} 
-                    setPlan={setPlan} 
-                    plan={plan} 
-                    setSavePlan={setSavePlan} 
-                    savePlan={savePlan}/>}
-                </div>
+  return (
+    <div>
+      <div
+        ref={side}
+        className={styles.side_btn}>
+        <span className="fas fa-bars"></span>
+        {/* <button onClick={() => toggleMenu()} className={styles.button}>
+          {isOpen ? <span>X</span> : <span>O</span>}
+        </button> */}
+      </div>
+      <nav className={styles.side_bar}>
+      <div className={styles.text}>
+        <Link to={"/"}>PLAN LIST</Link>
+        </div>
+        <ul>
+            <li>
+                <Link to={`/planning`}>
+                <span className="fas fa-caret-down"></span>
+                    Planning
+                </Link>
+            </li>          
+          {savePlan.map((item, idx) => (
+            <li value={item}>
+                <Link to={`/planning`} key={idx} title={item}>
+                <span className="fas fa-caret-down"></span>
+                    {item}
+                </Link>
+            </li>
+          ))}
+          {plan}
+        </ul>
+        <div className={styles.side_bottom}>
+          <div className={styles.bottom_content}>
+            <span className="fa-solid fa-plus"></span>
+            <span onClick={showModal}>새로만들기</span>
           </div>
         </div>
-      );
-}
+        
+      </nav>
+      <div>
+          {modalOpen && (
+            <NewNote
+              setModalOpen={setModalOpen}
+              setPlan={setPlan}
+              plan={plan}
+              setSavePlan={setSavePlan}
+              savePlan={savePlan}
+            />
+          )}
+        </div>
+    </div>
+  );
+};
 
-const comp = (prev, next) =>{
-    return prev.savePlan === next.savePlan
-}
-
+const comp = (prev, next) => {
+  return prev.savePlan === next.savePlan;
+};
 export default React.memo(SideBar, comp);
+

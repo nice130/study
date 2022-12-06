c04d0ca37f2bd1f3d866c27f8b04dd333f6f27a6
nice130 <nice130@naver.com>
2022-12-06 14:44:32 +0900

    트윗완성

--- HOON/nwitter/src/components/App.js
+++ HOON/nwitter/src/components/App.js
@@ -1,28 +1,38 @@
 import React, { useState, useEffect} from "react";
 import AppRouter from "./Router";
 import {authService} from "../fbase";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, onAuthStateChanged, updateCurrentUser, updateProfile } from "firebase/auth";
 
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
+  const [newName, setNewName] = useState("");
   useEffect(() => {
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setIsLoggedIn(true);
-        setUserObj(user);
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => updateProfile(user, { displayName: user.displayName }),
+          });
       } else {
         setIsLoggedIn(false);
       }
       setInit(true);
     });
   }, []);
+
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({...user});
+  }
   return (
     <>
       {init ?(
-        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} refreshUser={refreshUser} />
         ):(
           "Initializing..."
         )}

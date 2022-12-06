2368ff67795f2afad9c41707a263a579b91f13e4
nice130 <nice130@naver.com>
2022-12-06 14:43:36 +0900

    트윗 완성

--- HOON/nwitter/src/components/Router.js
+++ HOON/nwitter/src/components/Router.js
@@ -5,7 +5,7 @@ import Home from "../routes/Home";
 import Profile from "routes/Profile";
 import Navigation from "./Navigation";
 
-const AppRouter = ({isLoggedIn, userObj})=>{
+const AppRouter = ({isLoggedIn, userObj, refreshUser})=>{
     return(
         <Router>
             {isLoggedIn && <Navigation userObj={userObj} />}
@@ -17,7 +17,7 @@ const AppRouter = ({isLoggedIn, userObj})=>{
                             <Home userObj={userObj}/>
                         </Route>
                         <Route exact path="/profile">
-                            <Profile userObj={userObj}/>
+                            <Profile userObj={userObj} refreshUser={refreshUser}/>
                         </Route>
                         {/* <Redirect from="*" to="/" />  */}
                     </>

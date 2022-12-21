9d3ccae7540e0e0aa43da243e6573601c909e7b4
minosssss <mh.lee239@gmail.com>
2022-12-21 20:45:01 +0900

    SideBar 디자인 변경

--- project/planning/src/components/SideItems.js
+++ project/planning/src/components/SideItems.js
@@ -2,9 +2,9 @@ import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./new-sidebar.module.css";
 
-const SideItems = ({savePlan, setSavePlan}) => {
+const SideItems = ({ savePlan, setSavePlan }) => {
   const [addChild, setAddChild] = useState(false);
-   // $('.serv-btn').click(function (e) {
+  // $('.serv-btn').click(function (e) {
   //   $(e.target.nextElementSibling).toggleClass('show');
   //   $(e.target.children).toggleClass('rotate');
   // })
@@ -16,60 +16,65 @@ const SideItems = ({savePlan, setSavePlan}) => {
   const onAddChild = (e) => {
     const targetIdx = e.target.closest("li").dataset.index;
     savePlan[targetIdx].hasChild = true;
-      savePlan[targetIdx].child = [{
-        title : "TEST",
-        hasChild : false,
-    }]
-    setSavePlan ({
+    savePlan[targetIdx].child = [
+      {
+        title: "TEST",
+        hasChild: false,
+        onShow: false,
+      },
+    ];
+    setSavePlan({
       ...savePlan,
-    })
+    });
     setSavePlan([...savePlan]);
-  }
+  };
   const clickArrow = (e) => {
     e.target.classList.toggle("rotate");
-    e.target.nextElementSibling.toggle('show');
+    const targetIdx = e.target.closest("li").dataset.index;
+    savePlan[targetIdx].onShow = !savePlan[targetIdx].onShow;
+    setSavePlan({
+      ...savePlan,
+    });
+    setSavePlan([...savePlan]);
   };
-  console.log(savePlan);
   return (
-  <ul>
-    {savePlan.map((item, pindex) => (
-      <>
-      <li data-index={pindex}>
-        <Link 
-        to={`/planning`} 
-        key={pindex} 
-        title={item.title}>
-          <span 
-          className="fas fa-caret-right" 
-          onClick={clickArrow}></span>
-          {item.title}
-          <div className={styles.addList} onClick={onAddChild}>+</div>
-        </Link>
-      </li>
-      {item.hasChild ? <ul className="serv-show show">
-    {item.child.map((item, idx) => (
-      <>
-      <li data-index={idx} data-p-index={pindex}>
-        <Link 
-        to={`/`} 
-        key={idx} 
-        title={item.title}>
-          {/* <span 
+    <ul>
+      {savePlan.map((item, pindex) => (
+        <>
+          <li data-index={pindex}>
+            <Link to={`/planning`} key={pindex} title={item.title}>
+              <span className="fas fa-caret-right" onClick={clickArrow}></span>
+              {item.title}
+              <div className={styles.addList} onClick={onAddChild}>
+                +
+              </div>
+            </Link>
+          </li>
+          {item.hasChild ? (
+            <ul className={item.onShow? "serv-show show" : ""}>
+              {item.child.map((item, idx) => (
+                <>
+                  <li data-index={idx} data-p-index={pindex}>
+                    <Link to={`/`} key={idx} title={item.title}>
+                      {/* <span 
           className="fas fa-caret-right" 
           onClick={clickArrow}></span> */}
-          {item.title}
-          <div className={styles.addList} onClick={onAddChild}>+</div>
-        </Link>
-      </li>
-      </>
-    
-    ))}
-  </ul>  : ""}
-      </>
-    
-    ))}
-  </ul>)
+                      {item.title}
+                      <div className={styles.addList} onClick={onAddChild}>
+                        +
+                      </div>
+                    </Link>
+                  </li>
+                </>
+              ))}
+            </ul>
+          ) : (
+            ""
+          )}
+        </>
+      ))}
+    </ul>
+  );
 };
-
 
 export default SideItems;
